da2d6dc7567782706c3ac70ec81fd7f24113b7a7
André Almeida <andrejunior573@gmail.com>
2022-12-13 19:42:32 -0300

    Refazer Tabela Fato

--- ETL Modelo de Dados/ScriptsSQL/DatawarehouseSucos/Tabelas/Fato_3.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Fato_3]
-(
-    [Cod_Fabrica] NVARCHAR(50) NOT NULL, 
-    [Cod_Dia] NVARCHAR(50) NOT NULL, 
-    [Custo_Fixo] FLOAT NULL,
-    PRIMARY KEY ([Cod_Dia], [Cod_Fabrica]),
-    CONSTRAINT [FK_Fato_3_Dim_Fabrica] FOREIGN KEY ([Cod_Fabrica]) REFERENCES [Dim_Fabrica]([Cod_Fabrica]),
-    CONSTRAINT [FK_Fato_3_Dim_Tempo] FOREIGN KEY ([Cod_Dia]) REFERENCES [Dim_Tempo]([Cod_Dia])	
-)
